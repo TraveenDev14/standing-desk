bf38b98917ccbf6c56697bee68fa0f2e25130ce3
TraveenDev14 <traveen2006@gmail.com>
2024-08-02 17:21:32 +0530

    Add files via upload
    change if u like

---
+++ string1.py
@@ -0,0 +1,10 @@
+#split function
+b3 = "Ready for a furniture upgrade? Look no further! We're "
+print(b3[5])
+print(b3[0:4])
+# split seperator and maximum seperation
+print(b3.split('?', 5))
+# join function
+my_array1 = ["grapes","apples","pears","bannana"]
+print(my_array1)
+print(' and '.join(my_array1))
