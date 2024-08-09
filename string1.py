074643d706e041ca5c356d1e500c48333d51f08d
TraveenDev14 <traveen2006@gmail.com>
2024-08-09 17:47:03 +0530

    this is a mod done to string1.py

--- string1.py
+++ string1.py
@@ -8,3 +8,10 @@ print(b3.split('?', 5))
 my_array1 = ["grapes","apples","pears","bannana"]
 print(my_array1)
 print(' and '.join(my_array1))
+4
+# split seperator and maximum seperation
+print(b3.split('?', 5))
+# join function
+my_array1 = ["grapes","apples","pears","bannana"]
+print(my_array1)
+print(' and '.join(my_array1))
